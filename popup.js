b4dffe0a171de5eb36dc48d99d945183b106a01a
CatsUnited <Cats-United@users.noreply.github.com>
2015-09-13 18:37:00 +0800

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,4 @@
+// credit to an-OK-squirrel
+if ($("meta[content='noindex']").length > 0) {
+  $("#project > div:nth-child(1) > div:nth-child(1)").append('<div id="nfe" class="wip on" style="left:611px"><span>Not For Everyone</span></div>');
+}
